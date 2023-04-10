6249b32d4dad70bb1da685dbdf2498f84cb1b15f
letranbason <sonletranba2403@gmail.com>
2023-04-10 09:23:19 +0700

    ver3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,5 @@
-App = {
-    sliderPartner: function(){
-        jQuery('.partner_slider').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            variableWidth: true
-        });
-    },
-    sliderTable: function(){
-        jQuery('.slidertable').slick({
-            dots: false,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 1,
-            variableWidth: true
-        });
-    }
-};
-jQuery(document).ready(function(){
-    App.sliderPartner();
-    App.sliderTable();
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true
 });
